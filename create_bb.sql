e74d72aaf3d4c05f0469701b50e0619bceddcf01
nathaliedees <nathaliedees@hotmail.com>
2023-04-13 13:12:36 +0200

    update sql bb
    display only locations with data

--- create_bb.sql
+++ create_bb.sql
@@ -1,10 +1,12 @@
 drop table timeseries.boundingbox;
 create table timeseries.boundingbox as
-SELECT st_makeenvelope(min(st_x(geom)), min(st_y(geom)),max(st_x(geom)), max(st_y(geom)),28992),
+SELECT st_makeenvelope(min(st_x(l.geom)), min(st_y(l.geom)),max(st_x(l.geom)), max(st_y(geom)),28992) as bbgeom,
 left(name,3) as groupname,
-min(st_x(geom)) as minx,
-min(st_y(geom)) as miny,
-max(st_x(geom)) as maxx,
-max(st_y(geom)) as maxy 
-FROM timeseries.location
+min(st_x(l.geom)) as minx,
+min(st_y(l.geom)) as miny,
+max(st_x(l.geom)) as maxx,
+max(st_y(l.geom)) as maxy 
+FROM timeseries.location l
+join timeseries.timeseries ts on ts.locationkey = l.locationkey
+where l.geom is not Null
 group by left(name,3)
