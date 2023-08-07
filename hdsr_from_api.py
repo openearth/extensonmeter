e2ac0b4152a6c08f60ff863a2ae105968d3de112
hendriksen <gerrit.hendriksen@deltares.nl>
2023-08-07 09:31:23 +0200

    OpenEarth header - licentie added

--- hdsr_from_api.py
+++ hdsr_from_api.py
@@ -1,7 +1,38 @@
+"""" 
+#  Copyright notice
+#   --------------------------------------------------------------------
+#   Copyright (C) 2023 Deltares for NOBV/Somers project
+#   Nathalie Dees (nathalie.dees@deltares.nl)
+#
+#   This library is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this library.  If not, see <http://www.gnu.org/licenses/>.
+#   --------------------------------------------------------------------
+#
+# This tool is part of <a href="http://www.OpenEarth.eu">OpenEarthTools</a>.
+# OpenEarthTools is an online collaboration to share and manage data and
+# programming tools in an open source, version controlled environment.
+# Sign up to recieve regular updates of this function, and to contribute
+# your own tools.
+
+"""
+
 #Nathalie Dees
 #retrieving timeseries data from waterboards from Lizard API (v4!)~
 #before use, check current lizard version
 #later, data will be put in a database
+
+
+
 
 # %%
 import os
