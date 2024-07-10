98ef6b37a96ff36ad4046c4da28e1cd842d201e5
hendriksen <gerrit.hendriksen@deltares.nl>
2024-07-10 15:03:52 +0200

    mastermetadata created updated according to agreed number of columns

--- make_mastermetadata.py
+++ make_mastermetadata.py
@@ -50,40 +50,38 @@ import rasterio
 from utils_wcs import *
 from ts_helpders import establishconnection, testconnection
 
+# dictionary below is used to setup the mastertable that collects all data from the various schema's into 1 table
+
 dctcolumns = {}
 dctcolumns["well_id"] = "text"
 dctcolumns["aan_id"] = "text"
+dctcolumns["name"] = "text"
 dctcolumns["transect"] = "text"
 dctcolumns["parcel_type"] = "text"
-dctcolumns["x_centre_parcel"] = "double precision"
-dctcolumns["y_centre_parcel"] = "double precision"
+dctcolumns["ditch_id"] = "text"
+dctcolumns["ditch_name"] = "text"
 dctcolumns["soil_class"] = "text"
 dctcolumns["surface_level_m_nap"] = "double precision"
+dctcolumns["start_date"] = "text"
+dctcolumns["end_date"] = "text"
 dctcolumns["parcel_width_m"] = "double precision"
 dctcolumns["summer_stage_m_nap"] = "double precision"
 dctcolumns["winter_stage_m_nap"] = "double precision"
-dctcolumns["ditch_id"] = "text"
 dctcolumns["x_well"] = "double precision"
 dctcolumns["y_well"] = "double precision"
 dctcolumns["distance_to_ditch_m"] = "double precision"
-dctcolumns["distance_to_road_m"] = "double precision"
-dctcolumns["distance_to_railroad_m"] = "double precision"
-dctcolumns["distance_to_wis_m"] = "double precision"
-dctcolumns["start_date"] = "text"
-dctcolumns["end_date"] = "text"
-dctcolumns["records"] = "integer"
 dctcolumns["trenches"] = "double precision[]"
 dctcolumns["trench_depth_m_sfl"] = "double precision"
 dctcolumns["wis_distance_m"] = "double precision"
 dctcolumns["wis_depth_m_sfl"] = "double precision"
-# dctcolumns["source"] = "text"
-
-# TODO --> nadenken over het gebruik van source ipv well_id in de final master metadata
+dctcolumns["tube_top"] = "double precision"
+dctcolumns["tube_bot"] = "double precision"
+dctcolumns["geometry"] = "text"  # WKT represenation of the geom of the location
+dctcolumns["parcel_geom"] = "text"  # WKT represenation of the geom of the parcel
+dctcolumns["selection"] = "text"
 
 # globals
-geoserver_url = "https://service.pdok.nl/rws/ahn/wcs/v1_0"
-layername = "dtm_05m"
-cf = r"C:\projecten\grondwater_monitoring\nobv\2023\connection_online_qsomers.txt"
+cf = r"C:\develop\extensometer\connection_online.txt"
 session, engine = establishconnection(cf)
 
 if not testconnection(engine):
@@ -93,6 +91,7 @@ if not testconnection(engine):
 # key = tablename, value = columnname
 # every table is indicating the table with locations and has as value the column
 # that has measured surface elevation
+
 
 def preptable(tbl, columname, datatype):
     """alters a table in the database and adds a column with a specified datatype if not existing
@@ -109,105 +108,114 @@ def preptable(tbl, columname, datatype):
     engine.execute(strsql)
     return
 
+
 dcttable = {}
 dcttable["bro_timeseries.location"] = "placeholder"
 dcttable["hdsr_timeseries.location"] = "placeholder"
 dcttable["hhnk_timeseries.location"] = "placeholder"
 dcttable["wskip_timeseries.location"] = "placeholder"
-dcttable["waterschappen_timeseries.location"] = "placeholder" #handmetingen
+dcttable["waterschappen_timeseries.location"] = "placeholder"  # handmetingen
 dcttable["nobv_timeseries.location"] = "placeholder"  # nobv handmatige bewerkingen data
 
 
-nwtbl = 'metadata_ongecontroleerd.gwm'
-strsql = f"""drop table {nwtbl}; 
-create table if not exists {nwtbl} (source text primary key)"""
+nwtbl = "metadata_ongecontroleerd.gwm"
+strsql = f"""drop table if exists {nwtbl}; 
+create table if not exists {nwtbl} (source serial primary key);"""
 engine.execute(strsql)
 print("table created", nwtbl)
 for columname in dctcolumns.keys():
+    print(columname)
     preptable(nwtbl, columname, dctcolumns[columname])
-preptable(nwtbl, 'ditch_name', 'text')
-preptable(nwtbl, 'veenperceel', 'boolean')
-preptable(nwtbl, 'name', 'text')
-preptable(nwtbl, 'filterdepth', 'double precision')
-preptable(nwtbl, 'geom', 'geometry(POINT, 28992)')
 
 for tbl in dcttable.keys():
-    n=tbl.split('_')[0]
-
-    if n == 'nobv' or n == 'waterschappen':
-        strsql=f"""insert into {nwtbl} (source, name, filterdepth, 
-        parcel_width_m, summer_stage_m_nap, winter_stage_m_nap, 
-        trenches, trench_depth_m_sfl, x_centre_parcel, y_centre_parcel, 
-        surface_level_m_nap, ditch_id, distance_to_ditch_m,distance_to_road_m, 
-        distance_to_railroad_m, distance_to_wis_m, soil_class, x_well, y_well, geom) 
-            SELECT ('{n}_'||l.locationkey::text) as source, 
-            l.name, l.filterdepth, 
-            mt.parcel_width_m, 
-            mt.summer_stage_m_nap, 
-            mt.winter_stage_m_nap, 
-            mt.trenches, 
-            mt.trench_depth_m_sfl, 
-            mt.x_centre_parcel, 
-            mt.y_centre_parcel, 
+    n = tbl.split("_")[0]
+    print("attempt to exectute queries for", n)
+    if n == "nobv" or n == "waterschappen":
+        strsql = f"""insert into {nwtbl} (well_id, name, aan_id, transect, parcel_type, ditch_id, ditch_name, soil_class, surface_level_m_nap, start_date, end_date, parcel_width_m, summer_stage_m_nap, winter_stage_m_nap, x_well, y_well, distance_to_ditch_m, trenches, trench_depth_m_sfl, wis_distance_m, wis_depth_m_sfl, tube_top, tube_bot, geometry, parcel_geom, selection)
+        SELECT ('{n}_'||l.locationkey::text) as well_id, 
+            l.name, 
+            i.aan_id,
+            '' as transect,
+            'ref' as parcel_type,
+            mt.ditch_id,
+            '' as ditch_name, 
+            i.archetype, 
             mt.surface_level_m_nap, 
-            mt.ditch_id, 
-            mt.distance_to_ditch_m,
-            mt.distance_to_road_m, 
-            mt.distance_to_railroad_m, 
-            mt.distance_to_wis_m, 
-            mt.soil_class, 
+            mt.start_date,
+            mt.end_date,
+            mt.parcel_width_m, 
+            mt.summer_stage_m_nap,
+            mt.winter_stage_m_nap, 
             mt.x_well,
-            mt.y_well, geom FROM {n}_timeseries.location l
-            JOIN {n}_timeseries.location_metadata mt on mt.well_id = l.locationkey
-            JOIN {n}_timeseries.timeseries t on t.locationkey = l.locationkey
-            JOIN {n}_timeseries.parameter p on p.parameterkey = t.parameterkey
+            mt.y_well, 
+            mt.distance_to_ditch_m,
+            mt.trenches,
+            mt.trench_depth_m_sfl,
+            mt.wis_distance_m,
+            mt.wis_depth_m_sfl,
+            l.tubetop, 
+            l.tubebot,
+            st_astext(l.geom),
+            st_astext(st_force2d(i.geom)),
+            'yes' as selection
+            FROM bro_timeseries.location l
+            JOIN bro_timeseries.location_metadata mt on mt.well_id = l.locationkey
+            JOIN bro_timeseries.timeseries t on t.locationkey = l.locationkey
+            JOIN bro_timeseries.parameter p on p.parameterkey = t.parameterkey
+            JOIN public.input_parcels_2022 i on st_within(l.geom,i.geom)
             where p.id = 'GWM'
             ON CONFLICT(source)
             DO NOTHING;"""
         engine.execute(strsql)
-        
+
     else:
-        strsql=f"""insert into {nwtbl} (source, name, filterdepth, 
-        parcel_width_m, summer_stage_m_nap, winter_stage_m_nap, 
-        trenches, trench_depth_m_sfl, x_centre_parcel, y_centre_parcel, 
-        surface_level_m_nap, ditch_id, distance_to_ditch_m,distance_to_road_m, 
-        distance_to_railroad_m, distance_to_wis_m, soil_class, x_well, y_well, geom) 
-            SELECT ('{n}_'||l.locationkey::text) as source, 
-            l.name, l.filterdepth, 
-            mt.parcel_width_m, 
-            mt.summer_stage_m_nap, 
-            mt.winter_stage_m_nap, 
-            mt.trenches, 
-            mt.trench_depth_m_sfl, 
-            mt.x_centre_parcel, 
-            mt.y_centre_parcel, 
-            mt.surface_level_m_nap, 
-            mt.ditch_id, 
-            mt.distance_to_ditch_m,
-            mt.distance_to_road_m, 
-            mt.distance_to_railroad_m, 
-            mt.distance_to_wis_m, 
-            mt.soil_class, 
-            mt.x_well,
-            mt.y_well, geom FROM {n}_timeseries.location l
-            JOIN {n}_timeseries.location_metadata mt on mt.well_id = l.locationkey
-            ON CONFLICT(source)
-            DO NOTHING;"""
+        strsql = f"""insert into {nwtbl} (well_id, name, aan_id, transect, parcel_type, ditch_id, ditch_name, soil_class, surface_level_m_nap, start_date, end_date, parcel_width_m, summer_stage_m_nap, winter_stage_m_nap, x_well, y_well, distance_to_ditch_m, trenches, trench_depth_m_sfl, wis_distance_m, wis_depth_m_sfl, tube_top, tube_bot, geometry, parcel_geom, selection)
+        SELECT ('{n}_'||l.locationkey::text) as well_id,
+        l.name, 
+        i.aan_id,
+        '' as transect,
+        'ref' as parcel_type,
+        mt.ditch_id,
+        '' as ditch_name, 
+        i.archetype, 
+        mt.surface_level_m_nap, 
+        mt.start_date,
+        mt.end_date,
+        mt.parcel_width_m, 
+        mt.summer_stage_m_nap,
+        mt.winter_stage_m_nap, 
+        mt.x_well,
+        mt.y_well, 
+        mt.distance_to_ditch_m,
+        mt.trenches,
+        mt.trench_depth_m_sfl,
+        mt.wis_distance_m,
+        mt.wis_depth_m_sfl,
+        l.tubetop, 
+        l.tubebot,
+        st_astext(l.geom),
+        st_astext(st_force2d(i.geom)),
+        'yes'
+        FROM hhnk_timeseries.location l
+        JOIN hhnk_timeseries.location_metadata mt on mt.well_id = l.locationkey
+        JOIN public.input_parcels_2022 i on st_within(l.geom,i.geom)
+        ON CONFLICT(source)
+        DO NOTHING;"""
         engine.execute(strsql)
 
-nwtbl = 'metadata_ongecontroleerd.swm'
+nwtbl = "metadata_ongecontroleerd.swm"
 strsql = f"""drop table {nwtbl}; 
 create table if not exists {nwtbl} (source text primary key)"""
 engine.execute(strsql)
 print("table created", nwtbl)
-preptable(nwtbl, 'name', 'text')
-preptable(nwtbl, 'geom', 'geometry(POINT, 28992)')
+preptable(nwtbl, "name", "text")
+preptable(nwtbl, "geom", "geometry(POINT, 28992)")
 
 for tbl in dcttable.keys():
-    n=tbl.split('_')[0]
-    if n == 'nobv' or n == 'waterschappen': 
+    n = tbl.split("_")[0]
+    if n == "nobv" or n == "waterschappen":
         print(n)
-        strsql=f"""insert into {nwtbl} (source, name, geom)
+        strsql = f"""insert into {nwtbl} (source, name, geom)
             SELECT ('{n}_'||l.locationkey::text) as source, l.name, l.geom FROM {n}_timeseries.location l
             JOIN {n}_timeseries.location_metadata mt on mt.well_id = l.locationkey
             JOIN {n}_timeseries.timeseries t on t.locationkey = l.locationkey
@@ -217,19 +225,19 @@ for tbl in dcttable.keys():
         engine.execute(strsql)
 
 
-strsql= f"""update metadata_ongecontroleerd.gwm gw
+strsql = f"""update metadata_ongecontroleerd.gwm gw
 set veenperceel = TRUE
 from public.input_parcels_2022 ip
 WHERE ST_Contains(ip.geom, gw.geom);"""
 engine.execute(strsql)
 
-strsql=f"""drop table metadata_ongecontroleerd.all_locations; 
+strsql = f"""drop table metadata_ongecontroleerd.all_locations; 
 create table metadata_ongecontroleerd.all_locations as
 select * from metadata_ongecontroleerd.gwm
 where veenperceel = True and distance_to_railroad_m > 10 and distance_to_road_m > 10 and distance_to_ditch_m > 5;"""
 engine.execute(strsql)
 
-strsql=f"""WITH updated_values AS (
+strsql = f"""WITH updated_values AS (
     SELECT DISTINCT ON (l.source) 
         l.source AS all_source, 
         swm.source AS swm_source,
@@ -256,16 +264,16 @@ FROM updated_values
 WHERE metadata_ongecontroleerd.all_locations.source = updated_values.all_source;"""
 engine.execute(strsql)
 
-strsql=f"""drop table metadata_ongecontroleerd.kalibratie; 
+strsql = f"""drop table metadata_ongecontroleerd.kalibratie; 
 create table metadata_ongecontroleerd.kalibratie as
 select * from metadata_ongecontroleerd.all_locations
 where ditch_name is not Null;"""
 engine.execute(strsql)
 
-strsql=f"""drop table metadata_ongecontroleerd.validatie;
+strsql = f"""drop table metadata_ongecontroleerd.validatie;
 create table metadata_ongecontroleerd.validatie as
 select * from metadata_ongecontroleerd.all_locations
 where ditch_id is Null;"""
 engine.execute(strsql)
 
-print('created table kalibratie, validatie')
+print("created table kalibratie, validatie")
