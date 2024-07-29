8fffba6e999d80d817ac84c8d3065da35da0dcd9
nathaliedees <nathaliedees@hotmail.com>
2024-07-29 15:38:37 +0200

    change folder structure - need to change codes

--- orm_loadtimeseries.py
+++ orm_loadtimeseries.py
@@ -41,7 +41,7 @@ PostgreSQL/PostGIS
 from sqlalchemy import create_engine
 
 ## Declare a Mapping to the database
-from orm_timeseries_nobv import Base
+from orm_timeseries_waterschappen import Base
 
 def checkschema(engine,schema):
     strsql = 'create schema if not exists {s}'.format(s=schema)
@@ -64,11 +64,11 @@ def dropdb(engine):
     Base.metadata.drop_all(engine)
 
 def resetindex(engine,schema):
-    strsql = """SELECT tablename, indexname, indexdef FROM pg_indexes WHERE schemaname = 'nobv_timeseries' ORDER BY tablename,indexname;"""
+    strsql = """SELECT tablename, indexname, indexdef FROM pg_indexes WHERE schemaname = 'waterschappen_timeseries' ORDER BY tablename,indexname;"""
     lst = engine.execute(strsql)
     for i in lst:
         print(i.tablename,i.indexname)
-        strSql = """ALTER SEQUENCE nobv_timeseries.{indx} RESTART WITH 1""".format(indx=i.indexname)
+        strSql = """ALTER SEQUENCE waterschappen_timeseries.{indx} RESTART WITH 1""".format(indx=i.indexname)
         engine.execute(strSql)
         
 if __name__ == "__main__":
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     # schemas = ('subsurface_second')
     # for schema in schemas:
     #     checkschema(engine,schema)
-    lschema = ('nobv_timeseries',)
+    lschema = ('waterschappen_timeseries',)
     for schema in lschema:
         checkschema(engine,schema)
     # format is #postgres://user:password@hostname/database (in this case hydrodb)    
